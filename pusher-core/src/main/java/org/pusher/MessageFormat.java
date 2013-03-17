ccb8ab5e248e26bc4777798746fbc486344911a6
bspies <brennanspies@gmail.com>
2013-03-16 22:29:33 -0700

    Added "asContentType" method

--- pusher-core/src/main/java/org/pusher/MessageFormat.java
+++ pusher-core/src/main/java/org/pusher/MessageFormat.java
@@ -3,6 +3,9 @@ package org.pusher;
 import javax.activation.MimeType;
 
 /**
+ * Class describing the format of the notification to be sent to the
+ * {@link org.pusher.destination.Destination Destination}.
+ *
  * @author Brennan Spies
  */
 public class MessageFormat {
@@ -10,20 +13,64 @@ public class MessageFormat {
     private String characterSet;
     private MimeType mimeType;
 
+    /**
+     * Creates a message format with the desired character encoding and
+     * MIME type of the notification.
+     * @param characterSet The character encoding of the notification
+     * @param mimeType The MIME type of the notification
+     */
     public MessageFormat(String characterSet, MimeType mimeType) {
         this.characterSet = characterSet;
         this.mimeType = mimeType;
     }
 
+    /**
+     * Creates a message format with the desired MIME type of the notification.
+     * @param mimeType The MIME type
+     */
     public MessageFormat(MimeType mimeType) {
-        this(null, mimeType);
+        this(null, mimeType);  //todo default charset?
     }
 
+    /**
+     * Returns the character set encoding of the format.
+     * @return The character set
+     */
     public String getCharacterSet() {
         return characterSet;
     }
 
+    /**
+     * Returns the MIME type of the message format.
+     * @return The MIME type
+     */
     public MimeType getMimeType() {
         return mimeType;
+    }
+
+    /**
+     * Returns a representation of this message format as
+     * a "Content-Type" HTTP header.
+     * @return The message format as "Content-Type" header
+     */
+    public String asContentType() {
+        return String.format("%; charset=%s", mimeType, characterSet); //todo handle charset null/default
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MessageFormat that = (MessageFormat) o;
+        return !(characterSet != null ? !characterSet.equals(that.characterSet) : that.characterSet != null)
+                && mimeType.equals(that.mimeType);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = characterSet != null ? characterSet.hashCode() : 0;
+        result = 31 * result + mimeType.hashCode();
+        return result;
     }
 }
