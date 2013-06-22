cba1d859ebb9abf5cf5568808a512a774893bad9
bspies <brennanspies@gmail.com>
2013-06-22 11:44:41 -0700

    Enum of mobile platforms

---
+++ pusher-core/src/main/java/org/pusher/delivery/mobile/MobilePlatform.java
@@ -0,0 +1,17 @@
+package org.pusher.delivery.mobile;
+
+/**
+ * @author Brennan Spies
+ */
+public enum MobilePlatform {
+    ANDROID,
+    IOS,
+    BLACKBERRY,
+    WINDOWS_PHONE,
+    WINDOWS_RT,
+    WEB_OS,
+    SYMBIAN,
+    PALM,
+    BADA,
+    OTHER
+}
